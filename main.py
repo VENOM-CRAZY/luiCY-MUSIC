6b2793f0bf43816d9ae5d29731d2ed929cbe1818
VENOMxCRAZY9 <97359676+VENOMxCRAZY9@users.noreply.github.com>
2022-02-20 23:54:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ Client(
 ).start()
 
 
-print(f"[INFO]: LUCY STARTED AS {BOT_NAME}!")
-print(f"[INFO]: LUCY ASSISTANT STARTED AS {ASSNAME}!")
+print(f"[INFO]:  STARTED AS {BOT_NAME}!")
+print(f"[INFO]:  ASSISTANT STARTED AS {ASSNAME}!")
 
 
 
